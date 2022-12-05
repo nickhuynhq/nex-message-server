5479ef3ed423e36c7ac24f5428beb6bd5685da1b
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-05 14:38:15 -0500

    Add Prisma create and validator to Conversation resolvers

--- src/graphql/resolvers/conversation.ts
+++ src/graphql/resolvers/conversation.ts
@@ -1,4 +1,6 @@
 import { GraphQLContext } from "../../util/types";
+import { ApolloError } from "apollo-server-core";
+import { Prisma } from "@prisma/client";
 
 const resolvers = {
   Mutation: {
@@ -7,9 +9,65 @@ const resolvers = {
       args: { participantIds: Array<string> },
       context: GraphQLContext
     ) => {
-      console.log("INSIDE CREATE CONVERSATION", args);
+      const { session, prisma } = context;
+      const { participantIds } = args;
+
+      if (!session?.user) {
+        throw new ApolloError("Not Authorized");
+      }
+
+      const {
+        user: { id: userId },
+      } = session;
+
+      try {
+        const conversation = await prisma.conversation.create({
+          data: {
+            participants: {
+              createMany: {
+                data: participantIds.map((id) => ({
+                  userId: id,
+                  hasSeenLatestMessage: id === userId,
+                })),
+              },
+            },
+          },
+          // indicate what fields you want back
+          include: conversationPopulated,
+        });
+      } catch (error) {
+        console.log("createConversation error", error);
+        throw new ApolloError("Error creating conversation");
+      }
     },
   },
 };
+
+export const participantPopulated =
+  Prisma.validator<Prisma.ConversationParticipantInclude>()({
+    user: {
+      select: {
+        id: true,
+        username: true,
+      },
+    },
+  });
+
+export const conversationPopulated =
+  Prisma.validator<Prisma.ConversationInclude>()({
+    participants: {
+      include: participantPopulated,
+    },
+    latestMessage: {
+      include: {
+        sender: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+      },
+    },
+  });
 
 export default resolvers;
