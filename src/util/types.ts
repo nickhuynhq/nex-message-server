39397215d72a0a244b1798a946c3683e7d22643d
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-06 12:47:40 -0500

    Create Conversations types

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,30 +1,42 @@
-import { PrismaClient } from "@prisma/client";
-import {ISODateString} from "next-auth"
+import { Prisma, PrismaClient } from "@prisma/client";
+import { ISODateString } from "next-auth";
+import {
+  conversationPopulated,
+  participantPopulated,
+} from "../graphql/resolvers/conversation";
 
 export interface GraphQLContext {
-    session: Session | null;
-    prisma: PrismaClient;
-    // pubsub
+  session: Session | null;
+  prisma: PrismaClient;
+  // pubsub
 }
 
 // Users
 export interface Session {
-    user: User;
-    expires: ISODateString;
+  user: User;
+  expires: ISODateString;
 }
 
 export interface User {
-    id: string;
-    username: string;
-    email: string;
-    emailVerified: boolean;
-    image: string;
-    name: string;
+  id: string;
+  username: string;
+  email: string;
+  emailVerified: boolean;
+  image: string;
+  name: string;
 }
 
 export interface CreateUsernameResponse {
-    success?: boolean;
-    error?: string;
+  success?: boolean;
+  error?: string;
 }
 
-//
+// Conversations
+
+export type ConversationPopulated = Prisma.ConversationGetPayload<{
+  include: typeof conversationPopulated;
+}>;
+
+export type ParticipantPopulated = Prisma.ConversationParticipantGetPayload<{
+  include: typeof participantPopulated;
+}>;
