820961e6d63f7e2b73a5b92b9bda67f332360337
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-06 11:53:53 -0500

    Create Messages typeDefs

--- src/graphql/typeDefs/conversation.ts
+++ src/graphql/typeDefs/conversation.ts
@@ -14,7 +14,7 @@ const typeDefs = gql`
 
   type Conversation {
     id: String
-    # latestMessage: Message
+    latestMessage: Message
     participants: [Participant]
     createdAt: Date
     updatedAt: Date
