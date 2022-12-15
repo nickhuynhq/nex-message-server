8fafeb31ddbb3626a2ea0b06040f7f37d2690de8
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-14 20:22:16 -0500

    Add scalar resolver to transfer date from db

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -2,7 +2,8 @@ import userResolvers from "./user";
 import merge from "lodash.merge";
 import conversationResolvers from "./conversation";
 import messageResolvers from "./messages"
+import scalarResolvers from "./scalars"
 
-const resolvers = merge({}, userResolvers, conversationResolvers, messageResolvers);
+const resolvers = merge({}, userResolvers, conversationResolvers, messageResolvers, scalarResolvers);
 
 export default resolvers;
